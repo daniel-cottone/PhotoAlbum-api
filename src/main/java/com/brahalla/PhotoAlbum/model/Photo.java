04f070eb2dbf801cd54b4a426ba3ae7093335bc0
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-02 08:36:34 -0500

    adding hibernate annotations for photos

--- src/main/java/com/brahalla/PhotoAlbum/model/Photo.java
+++ src/main/java/com/brahalla/PhotoAlbum/model/Photo.java
@@ -1,29 +1,51 @@
 package com.brahalla.PhotoAlbum.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "photos")
 public class Photo implements Serializable {
 
 	//private static final long serialVersionUID = ;
-	private Integer id;
+	@Id
+	@Column(name = "photo_id")
+	private Long id;
+
+	@Column(name = "title")
 	private String title;
+
+	@Column(name = "created_date")
+	private Date createdDate;
+
+	@Column(name = "file_path")
+	private String filePath;
+
+	@Column(name = "album_id")
 	private Integer albumId;
 
 	public Photo() {
 		super();
 	}
 
-	public Photo(Integer id, String name, Integer albumId) {
+	public Photo(Long id, String name, Date createdDate, String filePath, Long albumId) {
 		this.setId(id);
 		this.setTitle(title);
+		this.setCreatedDate(createdDate);
+		this.setFilePath(filePath);
 		this.setAlbumId(albumId);
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return this.id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -35,11 +57,27 @@ public class Photo implements Serializable {
 		this.title = title;
 	}
 
-	public Integer getAlbumId() {
+	public Date getCreatedDate() {
+		return this.createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getFilePath() {
+		return this.filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Long getAlbumId() {
 		return this.albumId;
 	}
 
-	public void setAlbumId(Integer id) {
+	public void setAlbumId(Long id) {
 		this.albumId = albumId;
 	}
 
@@ -53,6 +91,9 @@ public class Photo implements Serializable {
 			.append(super.toString()).append(tab)
 			.append("id = ").append(this.id).append(tab)
 			.append("title = ").append(this.title).append(tab)
+			.append("createdDate = ").append(this.createdDate).append(tab)
+			.append("filePath = ").append(this.filePath).append(tab)
+			.append("albumId = ").append(this.albumId).append(tab)
 			.append(" )");
 
 		return retValue.toString();
