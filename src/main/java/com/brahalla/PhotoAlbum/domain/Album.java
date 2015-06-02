cded333559933226c44872ee91a89c33b54640eb
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-02 15:02:53 -0500

    whoops

--- src/main/java/com/brahalla/PhotoAlbum/domain/Album.java
+++ src/main/java/com/brahalla/PhotoAlbum/domain/Album.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
