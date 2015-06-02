4e0d2cefa1cb81236c52b44e39526a1063acec39
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-02 08:24:28 -0500

    Adding hibernate annotations

--- src/main/java/com/brahalla/PhotoAlbum/model/Album.java
+++ src/main/java/com/brahalla/PhotoAlbum/model/Album.java
@@ -1,27 +1,48 @@
 package com.brahalla.PhotoAlbum.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "albums")
 public class Album implements Serializable {
 
 	//private static final long serialVersionUID = ;
-	private Integer id;
+	@Id
+	@Column(name = "album_id")
+	private Long id;
+
+	@Column(name = "title")
 	private String title;
+
+	@Column(name = "created_date")
+	private Date createdDate;
+
+	@Column(name = "cover_photo_id")
+	private Long coverPhotoId;
 
 	public Album() {
 		super();
 	}
 
-	public Album(Integer id, String title) {
+	public Album(Long id, String title, Date createdDate, Long coverPhotoId) {
 		this.setId(id);
 		this.setTitle(title);
+		this.setCreatedDate(createdDate);
+		this.setCoverPhotoId(coverPhotoId);
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return this.id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -31,6 +52,22 @@ public class Album implements Serializable {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public Date getCreatedDate() {
+		return this.createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Long getCoverPhotoId() {
+		return this.coverPhotoId;
+	}
+
+	public void setCoverPhotoId(Long coverPhotoId) {
+		this.coverPhotoId = coverPhotoId;
 	}
 
 	public String toString() {
@@ -43,6 +80,8 @@ public class Album implements Serializable {
 			.append(super.toString()).append(tab)
 			.append("id = ").append(this.id).append(tab)
 			.append("title = ").append(this.title).append(tab)
+			.append("createdDate = ").append(this.createdDate).append(tab)
+			.append("coverPhotoId = ").append(this.coverPhotoId).append(tab)
 			.append(" )");
 
 		return retValue.toString();
