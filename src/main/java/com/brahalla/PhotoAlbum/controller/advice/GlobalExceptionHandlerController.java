ee1cb373f24245145ed73072e59b7d1ae0487529
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-02 13:02:03 -0500

    Adding support for bad requests

--- src/main/java/com/brahalla/PhotoAlbum/controller/advice/GlobalExceptionHandlerController.java
+++ src/main/java/com/brahalla/PhotoAlbum/controller/advice/GlobalExceptionHandlerController.java
@@ -1,6 +1,7 @@
 package gov.tn.portal.vserv.controller.advice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,20 +9,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ExceptionController {
+public class GlobalExceptionHandlerController {
 
+  // No page mapping found
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler({NoHandlerFoundException.class})
+  @ResponseBody
+  public ResponseEntity<ErrorResponse> noHandlerFoundExceptionHandler(NoHandlerFoundException e) {
+    //log.debug("noHandlerFound: stacktrace={}", ExceptionUtils.getStackTrace(e));
+
+    String errorCode = "400 - Bad Request";
+    String errorMsg = "Requested URL doesn't exist";
+
+    return new ResponseEntity<>(new ErrorResponse(errorCode, errorMsg), HttpStatus.BAD_REQUEST);
+  }
+
+  // Bad JSON request
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler({HttpMessageNotReadableException.class})
 	@ResponseBody
-	public Error handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+	public Error httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
 		Error error = new Error("HttpMessageNotReadableException");
 		return error;
 	}
 
+  // Generic exception
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler({Exception.class})
   @ResponseBody
-	public Error handleException(Exception e) {
+	public Error exceptionHandler(Exception e) {
     Error error = new Error("Exception");
 		return error;
 	}
