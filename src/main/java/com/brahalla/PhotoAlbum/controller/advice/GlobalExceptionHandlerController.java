d54053c2e2e390674aa0ff08efd45841c44c2c2e
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-02 13:04:31 -0500

    Adding dependency

--- src/main/java/com/brahalla/PhotoAlbum/controller/advice/GlobalExceptionHandlerController.java
+++ src/main/java/com/brahalla/PhotoAlbum/controller/advice/GlobalExceptionHandlerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandlerController {
