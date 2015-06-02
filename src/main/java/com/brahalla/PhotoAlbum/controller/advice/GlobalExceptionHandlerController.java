a80290f92135d3fb00b6218cb681ad54b2b8bb93
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-02 14:30:00 -0500

    logging

--- src/main/java/com/brahalla/PhotoAlbum/controller/advice/GlobalExceptionHandlerController.java
+++ src/main/java/com/brahalla/PhotoAlbum/controller/advice/GlobalExceptionHandlerController.java
@@ -2,6 +2,9 @@ package gov.tn.portal.vserv.controller.advice;
 
 import com.brahalla.PhotoAlbum.model.ErrorResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -14,11 +17,14 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandlerController {
 
+	private static final Log log = LogFactory.getLog(GlobalExceptionhandlerController.class);
+
   // Bad JSON request
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler({HttpMessageNotReadableException.class})
 	@ResponseBody
 	public ResponseEntity<ErrorResponse> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
+		log.debug("httpMessageNotReadableExceptionHandler: stacktrace={}", ExceptionUtils.getStackTrace(e));
 		return new ResponseEntity<>(
 			new ErrorResponse(
 				"400 - Bad Request",
@@ -33,6 +39,7 @@ public class GlobalExceptionHandlerController {
 	@ExceptionHandler({Exception.class})
   @ResponseBody
 	public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+		log.debug("exceptionHandler: stacktrace={}", ExceptionUtils.getStackTrace(e));
 		return new ResponseEntity<>(
 			new ErrorResponse(
 				"500 - Internal Server Error",
