d3a474abd6c3a54ac55ef86d99fcb7c4bc824612
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-27 21:37:55 -0500

    decoupling config

--- src/main/java/com/brahalla/PhotoAlbum/configuration/ApplicationConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/ApplicationConfiguration.java
@@ -3,61 +3,11 @@ package com.brahalla.PhotoAlbum.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.brahalla.PhotoAlbum.dao.AccountRepository;
-import com.brahalla.PhotoAlbum.domain.entity.Account;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 @SpringBootApplication
 public class ApplicationConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationConfiguration.class, args);
 	}
-
-}
-
-@Configuration
-class GlobalAuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
-
-  @Autowired
-  AccountRepository accountRepository;
-
-  @Override
-  public void init(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-    authenticationManagerBuilder.userDetailsService(userDetailsService());
-  }
-
-  @Bean
-  UserDetailsService userDetailsService() {
-    return new UserDetailsService() {
-
-      @Override
-      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Account account = accountRepository.findByUsername(username);
-        if(account != null) {
-          return new User(
-            account.getUsername(),
-            account.getPassword(),
-            true, true, true, true,
-            AuthorityUtils.createAuthorityList("USER")
-          );
-        } else {
-          throw new UsernameNotFoundException("could not find the user '" + username + "'");
-        }
-      }
-
-    };
-  }
 
 }
