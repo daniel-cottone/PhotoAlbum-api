b617e60ca686862038cb626f10e373b55182eb6f
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 16:24:01 -0500

    testing

--- src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
@@ -5,8 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
