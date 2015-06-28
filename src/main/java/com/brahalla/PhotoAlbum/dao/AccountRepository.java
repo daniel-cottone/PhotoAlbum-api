5a758e86cf5dbf5f1aebed97e1220c946c1aa77c
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 07:25:39 -0500

    testing repository annotation

--- src/main/java/com/brahalla/PhotoAlbum/dao/AccountRepository.java
+++ src/main/java/com/brahalla/PhotoAlbum/dao/AccountRepository.java
@@ -3,7 +3,9 @@ package com.brahalla.PhotoAlbum.dao;
 import com.brahalla.PhotoAlbum.domain.entity.Account;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
   public Account findByUsername(String username);
