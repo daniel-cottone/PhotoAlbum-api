a474ad0e39d25e8e418f78056e74554b47189ae8
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 07:30:24 -0500

    testing enablejpa annotation

--- src/main/java/com/brahalla/PhotoAlbum/configuration/ApplicationConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/ApplicationConfiguration.java
@@ -2,8 +2,11 @@ package com.brahalla.PhotoAlbum.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ApplicationConfiguration {
 
 	public static void main(String[] args) {
