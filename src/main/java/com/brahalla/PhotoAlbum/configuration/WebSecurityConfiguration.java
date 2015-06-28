f9f767f7d89c44bb941d0fd6046ec9b2561648a9
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-27 20:49:50 -0500

    adding web security configuration

---
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.brahalla.PhotoAlbum.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().fullyAuthenticated()
+      .and().httpBasic()
+      .and().csrf().disable();
+  }
+
+}
