a5353e38942e00cf518f1e7a64d9e801d6894ab2
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-27 20:50:36 -0500

    updating class name

--- src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
