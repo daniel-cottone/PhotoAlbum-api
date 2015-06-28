fd45a771688cc61c21671d32bdbdb17d6608fe43
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 18:45:59 -0500

    removing web security options

--- src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
@@ -11,9 +11,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().fullyAuthenticated()
-      .and().httpBasic()
-      .and().csrf().disable();
+    http.authorizeRequests().anyRequest().fullyAuthenticated();
+      //.and().csrf().disable();
   }
 
 }
