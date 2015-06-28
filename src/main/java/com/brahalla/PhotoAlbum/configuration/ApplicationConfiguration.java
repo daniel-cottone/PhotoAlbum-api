04c7cc0bc595f10833c4e9ffac0156db424d9dc9
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 07:39:28 -0500

    removing jpa annotation

--- src/main/java/com/brahalla/PhotoAlbum/configuration/ApplicationConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/ApplicationConfiguration.java
@@ -2,11 +2,8 @@ package com.brahalla.PhotoAlbum.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.brahalla.PhotoAlbum.dao"})
 public class ApplicationConfiguration {
 
 	public static void main(String[] args) {
