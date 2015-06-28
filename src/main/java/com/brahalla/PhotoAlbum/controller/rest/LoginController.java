40f8f24351c205d7971b47844f97feda93d48bd7
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 18:20:09 -0500

    removing unnecessary imports

--- src/main/java/com/brahalla/PhotoAlbum/controller/rest/LoginController.java
+++ src/main/java/com/brahalla/PhotoAlbum/controller/rest/LoginController.java
@@ -5,14 +5,10 @@ import com.brahalla.PhotoAlbum.model.json.AccountInfo;
 import com.brahalla.PhotoAlbum.model.json.LoginResponse;
 import com.brahalla.PhotoAlbum.service.AccountService;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
