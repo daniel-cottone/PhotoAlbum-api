48006dbe0deb04309de9aed85edbcdd10505f432
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-27 20:40:48 -0500

    adding authentication support

---
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/GlobalAuthenticationConfiguration.java
@@ -0,0 +1,48 @@
+package com.brahalla.PhotoAlbum.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@Configuration
+public class GlobalAuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+  @Autowired
+  AccountRepository accountRepository;
+
+  @Override
+  public void init(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(userDetailsService());
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    return new UserDetailsService() {
+
+      @Override
+      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if(account != null) {
+          return new User(
+            account.getUsername(),
+            account.getPassword(),
+            true, true, true, true,
+            AuthorityUtils.createAuthorityList("USER")
+          );
+        } else {
+          throw new UsernameNotFoundException("could not find the user '" + username + "'");
+        }
+      }
+
+    };
+  }
+
+}
