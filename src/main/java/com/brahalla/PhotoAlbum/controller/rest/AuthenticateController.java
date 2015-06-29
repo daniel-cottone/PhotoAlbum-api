49b51e393b195d8b06dbb85cc7caeee477a7e4cc
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-29 10:55:13 -0500

    typo

--- src/main/java/com/brahalla/PhotoAlbum/controller/rest/AuthenticateController.java
+++ src/main/java/com/brahalla/PhotoAlbum/controller/rest/AuthenticateController.java
@@ -6,7 +6,7 @@ import com.brahalla.PhotoAlbum.model.json.response.LoginResponse;
 import com.brahalla.PhotoAlbum.service.AccountService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.codec.Base64;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
