4870467565556b860cd1c9cb4e846917ba5deb1e
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-29 08:30:35 -0500

    adding required import

--- src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
