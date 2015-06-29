dc8d6c26f7a7aac5f39b3ca537687124a6cbf1b4
Daniel Cottone <daniel.cottone@gmail.com>
2015-06-28 19:57:03 -0500

    typo

--- src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/brahalla/PhotoAlbum/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.brahalla.PhotoAlbum.dao.AccountRepository;
 import com.brahalla.PhotoAlbum.domain.entity.Account;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
